c2da08437bd7153f38cb052661bcb3d40d1d2eb7
Connell <cmcginley@gmail.com>
2022-03-28 22:08:21 +0100

    Delete tcpclient/target directory

--- tcpclient/target/rls/debug/deps/tcpclient-01e6b7ddd8badc7a.d
+++
@@ -1,5 +0,0 @@
-/Users/connell/rusty/tcpPlayground/tcpclient/target/rls/debug/deps/tcpclient-01e6b7ddd8badc7a.rmeta: tcpclient/src/main.rs
-
-/Users/connell/rusty/tcpPlayground/tcpclient/target/rls/debug/deps/tcpclient-01e6b7ddd8badc7a.d: tcpclient/src/main.rs
-
-tcpclient/src/main.rs:
--- tcpclient/target/rls/debug/deps/tcpclient-a9b8ba5112d04b1c.d
+++
@@ -1,5 +0,0 @@
-/Users/connell/rusty/tcpPlayground/tcpclient/target/rls/debug/deps/tcpclient-a9b8ba5112d04b1c.rmeta: tcpclient/src/main.rs
-
-/Users/connell/rusty/tcpPlayground/tcpclient/target/rls/debug/deps/tcpclient-a9b8ba5112d04b1c.d: tcpclient/src/main.rs
-
-tcpclient/src/main.rs:
